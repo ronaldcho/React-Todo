98c92d3dc4d5f7591482c6f163da4c9a386ab04e
Ronald Cho <ronaldcho@gmail.com>
2019-07-03 13:44:30 -0700

    Todo.js component

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,24 @@
+/* ########################################################################
+
+<Todo /> is a component that takes in the todo data and 
+displays the task to the screen.
+
+###########################################################################
+*/
+import React from "react";
+
+const Todo = props => {
+  console.log(`this is Todo `, props);
+  return (
+    <div
+      style={props.completed ? { textDecoration: "line-through" } : null}
+      onClick={event => {
+        props.handleToggleComplete(props.todo.id);
+      }}
+    >
+      <li> {props.taskname}</li>
+    </div>
+    // <li key={props.id}>{props.taskname}</li>
+  );
+};
+export default Todo;
